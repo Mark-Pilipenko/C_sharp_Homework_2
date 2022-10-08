7fa8f0fcba1037ed41a784bedb105d7bcd322431
Mark <kalmark@mail.ru>
2022-10-08 16:25:02 +0300

    Add Ex_10_Second_Digit

---
+++ Ex_10_Second_Digit/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трехзначное число: ");
+int noun = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+while (noun < 100|noun > 999)
+{
+    Console.WriteLine("Число не трехзначное!");
+    Console.Write("Введите трехзначное число: ");
+    noun = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+int second = (noun % 100 - noun % 10) / 10;
+Console.WriteLine("Вторая цифра введенного числа: " + second);
