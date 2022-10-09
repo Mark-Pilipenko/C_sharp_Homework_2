a0ff6d9f7037061a6691259587e232d7aa8017f0
Mark <kalmark@mail.ru>
2022-10-09 13:18:36 +0300

    Add Ex_15_Days

---
+++ Ex_15_Days/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите номер дня недели: ");
+int noun = Convert.ToInt32(Console.ReadLine());
+while (noun < 1|noun > 7)
+{
+    Console.WriteLine("Такого дня недели не существует!");
+    Console.Write("Введите номер дня недели: ");
+    noun = noun = Convert.ToInt32(Console.ReadLine());
+}
+if (noun <= 5 & noun >= 1) Console.WriteLine("Нет, не выходной:(");
+else Console.WriteLine("Да, выходной!");
+
+switch (noun)
+{
+    case 1:
+    Console.WriteLine("И вообще, понедельник:((");
+    break;
+    case 3:
+    Console.WriteLine("Среда - маленькая пятница:)");
+    break;
+    case 5:
+    Console.WriteLine("НО - ПЯТНИЦА!!!");
+    break;
+    case 7:
+    Console.WriteLine("Аккуратно - завтра на работу!");
+    break;
+}
